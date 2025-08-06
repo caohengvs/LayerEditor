792af1b59bdf22069f20468071e6174052bdb5a6
Curtis <caohengvs@163.com>
2025-08-06 16:50:58 +0800

    stop tracking userconfig、

--- UserConfig.cmake
+++
@@ -1,4 +0,0 @@
-set(Qt5_ROOT "D:/Qt/5.12.12/msvc2017_64")
-set(3rd_PARTY_ROOT "D:/repos/3rdParty")
-set(CMAKE_TOOLCHAIN_FILE "D:/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE STRING "vcpkg toolchain file")
-set(VCPKG_TARGET_TRIPLET "x64-windows" CACHE STRING "")
